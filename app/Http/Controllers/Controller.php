4c589580996bf0624dcc74192713032d62773840
SilverBullete <1256819071@qq.com>
2018-07-26 10:44:56 +0800

    返回Api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,14 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function apiReponse($code,$error,$data)
+    {
+        $json = [
+            'code' => $code,
+            'error' => $error,
+            'data' => $data
+        ];
+
+        return response()->json($json);
+    }
 }
