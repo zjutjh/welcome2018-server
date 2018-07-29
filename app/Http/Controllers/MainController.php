f6009b5dbb2b10a13b1c11e347f1d5e101ceadfc
SilverBullete <1256819071@qq.com>
2018-07-29 20:43:29 +0800

    更新查询寝室

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Request;
 
 class MainController extends Controller{
     public function searchStudentId(Request $request){
+        if($request){
+            return $this->apiReponse(201,'未接收到数据',null);
+        }
         $redis = app('redis.connection');
         $name = $request->get('name');
         $id_card = $request->get('pass');
@@ -19,8 +22,70 @@ class MainController extends Controller{
             }
         }
         else{
-            return $this->apiReponse(201,'此身份证不存在',null);
+            return $this->apiReponse(201,'查无此人',null);
         }
+    }
+
+    public function getClassmates(Request $request){
+        if($request){
+            return $this->apiReponse(201,'未接收到数据',null);
+        }
+        $redis = app('redis.connection');
+        $id_card = $request->get('id_card');
+        if ($student = $redis->get($id_card)){
+            $student = json_decode($student);
+            $class = $student->class;
+        }
+        else{
+            return $this->apiReponse(201,'查无此人',null);
+        }
+        $classmates = $redis->smembers($class);
+        return $this->apiReponse(200,null,['classmates'=>$classmates]);
+    }
+
+    public function searchDormitory(Request $request){
+        if($request){
+            return $this->apiReponse(201,'未接收到数据',null);
+        }
+        $redis = app('redis.connection');
+        $name = $request->get('name');
+        $id_card = $request->get('pass');
+        if($student = $redis->get($id_card)){
+            $student = json_decode($student);
+            if ($student->name == $name){
+                $student_id = $student->student_id;
+            }
+            else{
+                return $this->apiReponse(201,'身份证与姓名不匹配',null);
+            }
+        }
+        else{
+            return $this->apiReponse(201,'查无此人',null);
+        }
+        if ($dormitory = $redis->get($student_id)){
+            $dormitory = json_decode($dormitory);
+            return $this->apiReponse(200,null,$dormitory);
+        }
+        else{
+            return $this->apiReponse(201,'未查到寝室信息',null);
+        }
+    }
+
+    public function getRoommates(Request $request){
+        if($request){
+            return $this->apiReponse(201,'未接收到数据',null);
+        }
+        $redis = app('redis.connection');
+        $student_id = $request->get('student_id');
+        if ($dormitory = $redis->get($student_id)){
+            $dormitory = json_decode($dormitory);
+            $location = $dormitory->location;
+        }
+        else{
+            return $this->apiReponse(201,'查无此人',null);
+        }
+        $roommates = $redis->smembers($location);
+        return $this->apiReponse(200,null,['roommates'=>$roommates]);
     }
 }
 
--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Student_id;
+use App\Dormitory;
 
 class RedisController extends Controller{
 
-    public function save(){
+    public function first_save(){
         $redis = app('redis.connection');
-        $students_detail = DB::table('student_id')
-            ->select('id','name','id_card','student_id','class','head_teacher')
+        $students_detail = Student_id::
+            select('id','name','id_card','student_id','class','head_teacher')
             ->orderBy('student_id','asc')
             ->get();
         foreach ($students_detail as $value){
             $json = json_encode($value);
             $redis->set($value->id_card,$json);
             $redis->sadd($value->class,$json);
+        }
+    }
+    public function second_save(){
+        $redis = app('redis.connection');
+        $students_detail = Dormitory::
+        select('id','name','id_card','student_id','location','bed','bed_order','bed_longer')
+            ->orderBy('student_id','asc')
+            ->get();
+        foreach ($students_detail as $value){
+            $json = json_encode($value);
+            $redis->set($value->student_id,$json);
+            $redis->sadd($value->location,$json);
         }
     }
 }
