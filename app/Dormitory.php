7aa3a3fe61d4574c62be2a39767d92139746ade1
SilverBullete <1256819071@qq.com>
2018-07-29 20:32:27 +0800

    model

---
+++ app/Dormitory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dormitory extends Model{
+
+}
+/**
+ * Created by PhpStorm.
+ * User: 20170
+ * Date: 2018/7/28
+ * Time: 13:18
+ */
---
+++ app/Student_id.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student_id extends Model{
+    
+}
+/**
+ * Created by PhpStorm.
+ * User: 20170
+ * Date: 2018/7/28
+ * Time: 12:51
+ */
