e9e3b898a658b70604bbdeadacaf25dfc56e0443
SilverBullete <1256819071@qq.com>
2018-08-01 13:57:07 +0800

    更新了对request的判定

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,139 +2,132 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
-class MainController extends Controller{
-    public function searchStudentId(Request $request){
-        if(!$request){
-            return $this->apiReponse(201,'未接收到数据',null);
+class MainController extends Controller
+{
+    public function searchStudentId(Request $request)
+    {
+        if (!$name = $request->input('name') || !$id_card = $request->input('pass')) {
+            return $this->apiReponse(201, '未接收到数据', null);
         }
         $redis = app('redis.connection');
-        $name = $request->get('name');
-        $id_card = $request->get('pass');
-        if($student = $redis->get($id_card)){
+        if ($student = $redis->get($id_card)) {
             $student = json_decode($student);
-            if ($student->name == $name){
-                if ($qq_groups = $redis->smembers($student->hometown)){
-                    return $this->apiReponse(200,null,['student'=>$student,'qq_groups'=>$qq_groups]);
-                }
-                elseif ($student->hometown == '浙江省'){
-                    return $this->apiReponse(200,null,['student'=>$student,'qq_groups'=>$this->getZheJiangQqGroups($student)]);
-                }
-                else{
-                    return $this->apiReponse(200,null,['student'=>$student,'qq_groups'=>$qq_groups]);
+            if ($student->name == $name) {
+                if ($qq_groups = $redis->smembers($student->hometown)) {
+                    return $this->apiReponse(200, null, ['student' => $student, 'qq_groups' => $this->change($qq_groups)]);
+                } elseif ($student->hometown == '浙江省') {
+                    return $this->apiReponse(200, null, ['student' => $student, 'qq_groups' => $this->change($this->getZheJiangQqGroups($student))]);
+                } else {
+                    return $this->apiReponse(200, null, ['student' => $student, 'qq_groups' => $this->change($qq_groups)]);
                 }
 
+            } else {
+                return $this->apiReponse(201, '身份证与姓名不匹配', null);
             }
-            else{
-                return $this->apiReponse(201,'身份证与姓名不匹配',null);
-            }
-        }
-        else{
-            return $this->apiReponse(201,'查无此人',null);
+        } else {
+            return $this->apiReponse(201, '查无此人', null);
         }
     }
 
-    public function getClassmates(Request $request){
-        if(!$request){
-            return $this->apiReponse(201,'未接收到数据',null);
+    public function getClassmates(Request $request)
+    {
+        if (!$id_card = $request->get('id_card')) {
+            return $this->apiReponse(201, '未接收到数据', null);
         }
         $redis = app('redis.connection');
-        $id_card = $request->get('id_card');
-        if ($student = $redis->get($id_card)){
+        if ($student = $redis->get($id_card)) {
             $student = json_decode($student);
             $class = $student->class;
-        }
-        else{
-            return $this->apiReponse(201,'查无此人',null);
+        } else {
+            return $this->apiReponse(201, '查无此人', null);
         }
         $classmates = $redis->smembers($class);
-        return $this->apiReponse(200,null,['classmates'=>$classmates]);
+        return $this->apiReponse(200, null, ['classmates' => $this->change($classmates)]);
     }
 
-    public function searchDormitory(Request $request){
-        if(!$request){
-            return $this->apiReponse(201,'未接收到数据',null);
+    public function searchDormitory(Request $request)
+    {
+        if (!$name = $request->get('name') || !$id_card = $request->get('pass')) {
+            return $this->apiReponse(201, '未接收到数据', null);
         }
         $redis = app('redis.connection');
-        $name = $request->get('name');
-        $id_card = $request->get('pass');
-        if($student = $redis->get($id_card)){
+
+        if ($student = $redis->get($id_card)) {
             $student = json_decode($student);
-            if ($student->name == $name){
+            if ($student->name == $name) {
                 $student_id = $student->student_id;
+            } else {
+                return $this->apiReponse(201, '身份证与姓名不匹配', null);
             }
-            else{
-                return $this->apiReponse(201,'身份证与姓名不匹配',null);
-            }
+        } else {
+            return $this->apiReponse(201, '查无此人', null);
         }
-        else{
-            return $this->apiReponse(201,'查无此人',null);
-        }
-        if ($dormitory = $redis->get($student_id)){
+        if ($dormitory = $redis->get($student_id)) {
             $dormitory = json_decode($dormitory);
-            return $this->apiReponse(200,null,$dormitory);
-        }
-        else{
-            return $this->apiReponse(201,'未查到寝室信息',null);
+            return $this->apiReponse(200, null, $dormitory);
+        } else {
+            return $this->apiReponse(201, '未查到寝室信息', null);
         }
     }
 
-    public function getRoommates(Request $request){
-        if(!$request){
-            return $this->apiReponse(201,'未接收到数据',null);
+    public function getRoommates(Request $request)
+    {
+        if (!$student_id = $request->get('student_id')) {
+            return $this->apiReponse(201, '未接收到数据', null);
         }
         $redis = app('redis.connection');
-        $student_id = $request->get('student_id');
-        if ($dormitory = $redis->get($student_id)){
+        if ($dormitory = $redis->get($student_id)) {
             $dormitory = json_decode($dormitory);
             $location = $dormitory->location;
-        }
-        else{
-            return $this->apiReponse(201,'查无此人',null);
+        } else {
+            return $this->apiReponse(201, '查无此人', null);
         }
         $roommates = $redis->smembers($location);
-        return $this->apiReponse(200,null,['roommates'=>$roommates]);
+        return $this->apiReponse(200, null, ['roommates' => $roommates]);
     }
 
-    public function getZheJiangQqGroups($student){
+    public function change($list)
+    {
+        $newlist = [];
+        $index = 0;
+        foreach ($list as $value) {
+            $newlist[$index] = json_decode($value);
+            $index++;
+        }
+        return $newlist;
+    }
+
+    public function getZheJiangQqGroups($student)
+    {
         $redis = app('redis.connection');
-        $beginning = substr($student->id_card,0,6);
-        if (substr($beginning,0,4) == '3302'){
+        $beginning = substr($student->id_card, 0, 6);
+        if (substr($beginning, 0, 4) == '3302') {
             return $redis->smembers('宁波市');
-        }
-        elseif (substr($beginning,0,4) == '3302'){
+        } elseif (substr($beginning, 0, 4) == '3302') {
             return $redis->smembers('温州市');
-        }
-        elseif (substr($beginning,0,4) == '3304'){
+        } elseif (substr($beginning, 0, 4) == '3304') {
             return $redis->smembers('嘉兴市');
-        }
-        elseif (substr($beginning,0,4) == '3305'){
+        } elseif (substr($beginning, 0, 4) == '3305') {
             return $redis->smembers('湖州市');
-        }
-        elseif (substr($beginning,0,4) == '3306'){
+        } elseif (substr($beginning, 0, 4) == '3306') {
             return $redis->smembers('绍兴市');
-        }
-        elseif (substr($beginning,0,4) == '3325'){
+        } elseif (substr($beginning, 0, 4) == '3325') {
             return $redis->smembers('丽水地区');
-        }
-        elseif (substr($beginning,0,4) == '3307'){
-            if ($beginning == '330781'){
-                return $redis->smembers('兰溪市','金华市');
-            }
-            else{
+        } elseif (substr($beginning, 0, 4) == '3307') {
+            if ($beginning == '330781') {
+                return $redis->smembers('兰溪市', '金华市');
+            } else {
                 return $redis->smembers('金华市');
             }
-        }
-        elseif (substr($beginning,0,4) == '3310'){
-            if ($beginning == '331023'){
-                return $redis->smembers('天台县','台州市');
-            }
-            else{
+        } elseif (substr($beginning, 0, 4) == '3310') {
+            if ($beginning == '331023') {
+                return $redis->smembers('天台县', '台州市');
+            } else {
                 return $redis->smembers('台州市');
             }
-        }
-        else{
+        } else {
             return null;
         }
     }
--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -6,35 +6,41 @@ use App\Student_id;
 use App\Dormitory;
 use App\Qq_group;
 
-class RedisController extends Controller{
+class RedisController extends Controller
+{
 
-    public function first_save(){
+    public function first_save()
+    {
         $redis = app('redis.connection');
         $students_detail = Student_id::
-            select('id','name','id_card','hometown','student_id','class','head_teacher')
-            ->orderBy('student_id','asc')
+        select('id', 'name', 'id_card', 'hometown', 'student_id', 'class', 'head_teacher')
+            ->orderBy('student_id', 'asc')
             ->get();
-        foreach ($students_detail as $value){
+        foreach ($students_detail as $value) {
             $json = json_encode($value);
-            $redis->set($value->id_card,$json);
-            $redis->sadd($value->class,$json);
+            $redis->set($value->id_card, $json);
+            $redis->sadd($value->class, $json);
         }
-        $qq_groups = Qq_group::get();
-        foreach ($qq_groups as $value){
+        $qq_groups = Qq_group::
+        select('hometown', 'qq_group', 'name')
+            ->get();
+        foreach ($qq_groups as $value) {
             $json = json_encode($value);
-            $redis->sadd($value->hometown,$json);
+            $redis->sadd($value->hometown, $json);
         }
     }
-    public function second_save(){
+
+    public function second_save()
+    {
         $redis = app('redis.connection');
         $students_detail = Dormitory::
-        select('id','name','id_card','student_id','location','bed','bed_order','bed_longer')
-            ->orderBy('student_id','asc')
+        select('id', 'name', 'id_card', 'student_id', 'location', 'bed', 'bed_order', 'bed_longer')
+            ->orderBy('student_id', 'asc')
             ->get();
-        foreach ($students_detail as $value){
+        foreach ($students_detail as $value) {
             $json = json_encode($value);
-            $redis->set($value->student_id,$json);
-            $redis->sadd($value->location,$json);
+            $redis->set($value->student_id, $json);
+            $redis->sadd($value->location, $json);
         }
     }
 }
