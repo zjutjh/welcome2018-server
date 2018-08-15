a2b68c8b761679e6837e654a3aceb0348f1babfe
zzhutianyu <704735206@qq.com>
2018-08-15 11:13:46 +0800

    update

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,13 +4,13 @@
     <meta charset=utf-8>
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>精弘网络</title>
-    <link href=/static/css/app.da925e160e9cfbf013ca78a1a790b6b5.css rel=stylesheet>
+    <link href=/static/css/app.2d965b8cd4ca2dee1e648e4ffe666972.css rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
 <script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script>
 <script type=text/javascript src=/static/js/vendor.caeb896f4090dd854163.js></script>
-<script type=text/javascript src=/static/js/app.71fb978f4264cafa6100.js></script>
+<script type=text/javascript src=/static/js/app.79f5efe488de0052e161.js></script>git
 </body>
 </body>
 </html>
