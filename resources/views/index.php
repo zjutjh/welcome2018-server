f27dc4d5975bf7e6b94ee81f6a6013c86b8450d8
zzhutianyu <704735206@qq.com>
2018-08-15 16:35:01 +0800

    update

--- resources/views/index.php
+++ resources/views/index.php
@@ -23,12 +23,12 @@
                 a.persisted && r()
             }, !1) : f[g]("load", r, !1), f[g]("resize", r, !1), q()
         }(320, 10, 100);</script>
-    <link href=/static/css/app.ca7ab1c3f3f50e08e7a6013e5a755845.css rel=stylesheet>
+    <link href=/static/css/app.4ac6c2434d0935b95be3abd86db381f1.css rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
 <script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script>
 <script type=text/javascript src=/static/js/vendor.caeb896f4090dd854163.js></script>
-<script type=text/javascript src=/static/js/app.66830c44275a84a77452.js></script>
+<script type=text/javascript src=/static/js/app.5ae7f29f60b260bf61ad.js></script>
 </body>
 </html>
