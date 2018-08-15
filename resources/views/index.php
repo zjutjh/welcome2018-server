e4b122cb1c9a717e36975c0e15a19b7304561c5b
zzhutianyu <704735206@qq.com>
2018-08-15 15:31:43 +0800

    update

--- resources/views/index.php
+++ resources/views/index.php
@@ -4,13 +4,32 @@
     <meta charset=utf-8>
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>精弘网络</title>
-    <link href=/static/css/app.7071315f227fc895a34b9b60b3d71941.css rel=stylesheet>
+    <meta name=apple-mobile-web-app-capable content=yes>
+    <meta name=format-detection content="telephone=no">
+    <script>!function (a, b, c) {
+            function q() {
+                var d = Math.min((o ? e[h]().width : f.innerWidth) / (a / b), c);
+                d != p && (k.innerHTML = "html{font-size:" + d + "px!important;" + n + "}", p = d)
+            }
+
+            function r() {
+                clearTimeout(l), l = setTimeout(q, 500)
+            }
+
+            var l, d = document, e = d.documentElement, f = window, g = "addEventListener", h = "getBoundingClientRect",
+                i = "pageshow", j = d.head || d.getElementsByTagName("HEAD")[0], k = d.createElement("STYLE"),
+                m = "text-size-adjust:100%;", n = "-webkit-" + m + "-moz-" + m + "-ms-" + m + "-o-" + m + m, o = h in e, p = null;
+            a = a || 320, b = b || 16, c = c || 32, j.appendChild(k), d[g]("DOMContentLoaded", q, !1), "on" + i in f ? f[g](i, function (a) {
+                a.persisted && r()
+            }, !1) : f[g]("load", r, !1), f[g]("resize", r, !1), q()
+        }(320, 10, 100);</script>
+    <link href=/static/css/app.eb71424dbca420d280a4bfcfb4871a6b.css rel=stylesheet>
 </head>
 <body>
 <div id=app></div>
 <script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script>
 <script type=text/javascript src=/static/js/vendor.caeb896f4090dd854163.js></script>
-<script type=text/javascript src=/static/js/app.1b6931cd385e154521d5.js></script>
+<script type=text/javascript src=/static/js/app.f5fb37ebb1857d33824b.js></script>
 </body>
 </body>
 </html>
